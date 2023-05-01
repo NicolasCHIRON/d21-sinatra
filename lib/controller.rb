ab5fd36ea0bb07a1a9b702c2af0d02490c709a26
jace <nic.chiron@gmail.com>
2023-05-01 15:27:39 +0200

    première version

---
+++ lib/controller.rb
@@ -0,0 +1,25 @@
+require 'bundler'
+Bundler.require
+
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+  get '/' do
+    erb :index, locals: {gossips: Gossip.all}
+  end
+
+  get '/gossips/new/' do
+    erb :new_gossips
+  end
+
+  post '/gossips/new/' do
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    redirect '/'
+  end
+
+  get '/gossips/:id' do
+    erb :show, locals: {id: params['id'], gossip: Gossip.find(params['id'])}
+  end
+
+
+end
---
+++ lib/gossip.rb
@@ -0,0 +1,27 @@
+require 'csv'
+
+class Gossip
+  attr_accessor :author, :content
+
+  def initialize(author, content)
+    @author = author
+    @content = content
+  end
+
+  def save
+    CSV.open("./db/gossip.csv", "ab") {|csv| csv << [author, content]; csv.close}
+  end
+
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+
+  def self.find(id_gossip)
+    return Gossip.all[id_gossip.to_i - 1]
+  end
+
+end
